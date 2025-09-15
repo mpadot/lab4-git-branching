2365f96d70772476618df97cdb56ec41153dacdb
Miles Padot <155395178+mpadot@users.noreply.github.com>
2025-09-15 15:47:19 -0400

    added how character eliminates dragon and wins lifetime of gold

--- story.py
+++ story.py
@@ -11,6 +11,7 @@ def intro():
 def left_path():
     print("You walk left and find a mysterious glowing sword stuck in a stone.")
     print("You pick up the sword and begin fighting the dragon")
+    print("You slay the dragon and are granted a lifetime worth of gold")
     
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
