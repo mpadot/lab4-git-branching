005c4413df7cebcb8dad53cf5b4287402ffba518
Miles Padot <155395178+mpadot@users.noreply.github.com>
2025-09-15 15:46:23 -0400

    added how the character picks up sword and fights dragon

--- story.py
+++ story.py
@@ -10,7 +10,8 @@ def intro():
 
 def left_path():
     print("You walk left and find a mysterious glowing sword stuck in a stone.")
-
+    print("You pick up the sword and begin fighting the dragon")
+    
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
 
